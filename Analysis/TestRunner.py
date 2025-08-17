8936f3e559b1b57c66da76feca0297eb154a284e
Niklas Angert <n.angert@lipso.dev>
2025-08-17 16:07:47 +0200

    _v17 benchmark

--- Analysis/TestRunner.py
+++ Analysis/TestRunner.py
@@ -1,6 +1,7 @@
 ﻿from scripts_of_tribute.game import Game
 from pathlib import Path
 
+from BetterNet.BetterNN_Bot.BetterNetBot_v17 import BetterNetBot_v17
 from BetterNet.BetterNN_Bot.BetterNetBot_v13 import BetterNetBot_v13
 from BetterNet.BetterNN_Bot.BetterNetBot_v14 import BetterNetBot_v14
 from BetterNet.BetterNN_Bot.CompetitionBot_v1 import CompetitionBot_v1
@@ -16,18 +17,20 @@ def main():
 
     #bot1_path = Path('./good_models/BetterNet_v12/better_net_v12_21.pt')
     #bot1_path = Path('./good_models/TributeNet_v1/tribute_net_v21.pt')
-    bot1_path = Path('./ModelTrainingModels/_v13/tribute_net_v21.pt')
+    bot1_path = Path('./model/v17/tribute_net_v29.pt')
     bot2_path = Path('./good_models/BetterNet_v13/better_net_v13_19.pt')
     bot14_path = Path('./good_models/BetterNet_v14/better_net_v14_13.pt')
+    bot17_path = Path('../data/saved_models/BetterNet_v17/better_net_v26.pt')
 
     #bot2 = TributeBotV1(bot_name="TributeNet_1", evaluate=True, model_path=bot1_path)
     #bot1 = BetterNetBot_v13(model_path=bot1_path, bot_name="BetterNet_2", evaluate=True, save_trajectory=False)
     #bot2 = BetterNetBot_v13(model_path=bot2_path, bot_name="BetterNet_1", evaluate=True, save_trajectory=False)
-    bot1 = CompetitionBot_v1(model_path=bot1_path, bot_name="Comp_1", evaluate=True, instant_moves=True, remove_end_turn=True)
+    #bot1 = CompetitionBot_v1(model_path=bot1_path, bot_name="Comp_1", evaluate=True, instant_moves=True, remove_end_turn=True)
     #bot2 = CompetitionBot_v1(model_path=bot1_path, bot_name="Comp_2", evaluate=True, instant_moves=True, remove_end_turn=False)
     #bot2 = CompetitionBot_v1(model_path=bot2_path, bot_name="BetterNet_2", evaluate=True)
     #bot1 = BetterNetBot_v14(bot14_path, bot_name="BetterNet_2", evaluate=True, save_trajectory=False)
     #bot2 = RandomBot(bot_name="RandomBot")
+    bot1 = BetterNetBot_v17(model_path=bot17_path, bot_name='better_net_v17', evaluate=True, save_trajectory=False)
     bot2 = BaselineBot(bot_name="Baseline")
 
     game = Game()
